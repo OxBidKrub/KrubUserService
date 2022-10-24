c77ece5a1d51f3f217f3e6945223a6a24d80b8c1
thunthup <thunthup123@gmail.com>
2022-10-24 15:02:05 +0700

    chore: ignore dist folder

--- dist/entity/base/Base.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntity } from 'typeorm';
-export declare abstract class Base extends BaseEntity {
-    id: string;
-    isActive: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-}
--- dist/entity/bid.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Base } from "./base/Base.entity";
-import { User } from "./user.entity";
-import { AuctionItem } from "./auctionItem.entity";
-export declare class Bid extends Base {
-    Price: number;
-    time: Date;
-    user: User;
-    auctionItem: AuctionItem;
-}
